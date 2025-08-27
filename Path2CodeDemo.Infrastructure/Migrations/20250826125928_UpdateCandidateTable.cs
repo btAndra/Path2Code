448e3b8ca02cc9d57c6ef478dab12910f098e863
AndraBt <andra.elena.butnariu@gmail.com.com>
2025-08-27 11:02:06 +0300

    Update .gitignore

---
+++ Path2CodeDemo.Infrastructure/Migrations/20250826125928_UpdateCandidateTable.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Path2CodeDemo.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateCandidateTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "Candidates",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Version",
+                table: "Candidates",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Version",
+                table: "Candidates");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "Candidates",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
